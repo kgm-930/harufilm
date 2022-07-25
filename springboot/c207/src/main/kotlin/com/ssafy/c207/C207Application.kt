8ffac69f3a8b8e54c51e3b7ab461eb04ecdf8abd
kgm <72342542+kgm-930@users.noreply.github.com>
2022-07-25 09:54:42 +0900

    [#87] feat: create springboot

---
+++ springboot/c207/src/main/kotlin/com/ssafy/c207/C207Application.kt
@@ -0,0 +1,11 @@
+package com.ssafy.c207
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class C207Application
+
+fun main(args: Array<String>) {
+    runApplication<C207Application>(*args)
+}
