69d5225b711456cc60219a91f0c27e4ce12144a4
kgm <72342542+kgm-930@users.noreply.github.com>
2022-07-25 10:50:05 +0900

    [#138] feat: user DB

--- springboot/c207/src/main/kotlin/com/ssafy/c207/api/controller/UserController.kt
+++ springboot/c207/src/main/kotlin/com/ssafy/c207/api/controller/UserController.kt
@@ -4,20 +4,30 @@ import com.ssafy.c207.api.dto.User
 import com.ssafy.c207.api.repository.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
+
 @RestController
 @RequestMapping("/user")
 class UserController(
     val userRepository: UserRepository
 ) {
     @GetMapping()
-    fun getMembers(): ResponseEntity<*> {
-        val users = userRepository.findAll()
+    fun getMembers(@RequestBody data: User): Any {
+        val user = userRepository.findById(data.id)
 
-        return ResponseEntity.ok(users)
+        if (user.isEmpty) {
+            return false
+        } else {
+            if (user.get().pw != data.pw) {
+                return false
+            }
+        }
+
+        return true
     }
 
     @PostMapping()
-    fun setMember(@RequestBody user:User): ResponseEntity<*>? {
+    fun setMember(@RequestBody user: User): ResponseEntity<*>? {
         val user = userRepository.save(user)
 
         return ResponseEntity.ok(user)
