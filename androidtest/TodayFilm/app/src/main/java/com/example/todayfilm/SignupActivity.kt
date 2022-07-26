5f5a4d6f877a10ab8c0fce4bdf7aeccd3cdb5b00
seongdo Byun <dmdsns@naver.com>
2022-07-26 15:50:40 +0900

    BE signup 후 화면전환 TEST

--- androidtest/TodayFilm/app/src/main/java/com/example/todayfilm/SignupActivity.kt
+++ androidtest/TodayFilm/app/src/main/java/com/example/todayfilm/SignupActivity.kt
@@ -1,8 +1,12 @@
 package com.example.todayfilm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.example.todayfilm.data.SignupData
 import com.example.todayfilm.data.User
 import com.example.todayfilm.databinding.ActivitySignupBinding
@@ -30,6 +34,19 @@ class SignupActivity : AppCompatActivity() {
                     override fun onResponse(call: Call<SignupData>, response: Response<SignupData>) {
                         val result:SignupData? = response.body()
                         Log.d("결과","성공" + result?.result )
+                        if (result?.message == "성공") {
+                            var dialog = AlertDialog.Builder(this@SignupActivity)
+                            dialog.setTitle("회원가입 성공!")
+                            dialog.setMessage("회원가입이 정상적으로 완료되었습니다.")
+                            dialog.show()
+
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                val intent = Intent(this@SignupActivity, MainActivity::class.java)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                startActivity(intent)
+                                finish()
+                            }, 2000)
+                        }
                     }
 
                     override fun onFailure(call: Call<SignupData>, t: Throwable) {
