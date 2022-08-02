eee5e58695005f039917f7d4e649dbf11b68ef81
김혁주 <lyricism3002@gmail.com>
2022-08-02 10:42:27 +0900

    merge Err fix

--- Back/MERGE/harufilm/src/main/java/com/ssafy/harufilm/service/UserService.java
+++ Back/MERGE/harufilm/src/main/java/com/ssafy/harufilm/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ssafy.harufilm.service;
 
+import com.ssafy.harufilm.dto.account.SignupRequestDto;
 import com.ssafy.harufilm.entity.User;
 
 public interface UserService {
@@ -7,5 +8,8 @@ public interface UserService {
     User getuserbyId(String userid);
 
     User getuserbyPid(int userpid);
+
+    User userNewSave(SignupRequestDto signupRequestDto);
+
 
 }
--- Back/MERGE/harufilm/src/main/java/com/ssafy/harufilm/service/UserServiceImpl.java
+++ Back/MERGE/harufilm/src/main/java/com/ssafy/harufilm/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.harufilm.dto.account.SignupRequestDto;
 import com.ssafy.harufilm.entity.User;
 import com.ssafy.harufilm.repository.UserRepository;
 
@@ -29,5 +30,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getuserbyPid(int userpid) {
         return userRepository.findByUserpid(userpid).orElse(null);
+    }
+
+
+    @Transactional
+    @Override
+    public User userNewSave(SignupRequestDto signupRequestDto) {
+        User user = User.builder()
+                .userid(signupRequestDto.getUserid())
+                .userpassword(signupRequestDto.getUserpassword())
+                .roles("USER")
+                .build();
+
+        return userRepository.save(user);
     }
 }
