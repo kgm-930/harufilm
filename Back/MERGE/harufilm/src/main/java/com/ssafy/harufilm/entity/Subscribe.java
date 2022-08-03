7f8e1eb17e29a414131be922f996ec83696a0daa
김혁주 <lyricism3002@gmail.com>
2022-08-03 13:13:39 +0900

    struct base commit

---
+++ Back/MERGE/harufilm/src/main/java/com/ssafy/harufilm/entity/Subscribe.java
@@ -0,0 +1,25 @@
+package com.ssafy.harufilm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Subscribe {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private int subidx;
+}
