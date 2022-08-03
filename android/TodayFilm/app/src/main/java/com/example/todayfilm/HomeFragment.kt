3b88bbd7815dc2be8d8ed7d30c9e0a56039253f5
celpegor216 <celpegor216@gmail.com>
2022-08-03 22:19:44 +0900

    complete - select main image add

--- android/TodayFilm/app/src/main/java/com/example/todayfilm/HomeFragment.kt
+++ android/TodayFilm/app/src/main/java/com/example/todayfilm/HomeFragment.kt
@@ -3,14 +3,11 @@ package com.example.todayfilm
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import com.example.todayfilm.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(), View.OnClickListener {
@@ -26,7 +23,6 @@ class HomeFragment : Fragment(), View.OnClickListener {
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
