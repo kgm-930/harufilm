1f3c1ca7986bebe75020e289d5f5a9d7c0cded46
kgm <72342542+kgm-930@users.noreply.github.com>
2022-08-08 15:12:19 +0900

    Update FCMService.kt

--- android/TodayFilm/app/src/main/java/com/example/todayfilm/FCMService.kt
+++ android/TodayFilm/app/src/main/java/com/example/todayfilm/FCMService.kt
@@ -1,7 +1,8 @@
 package com.example.todayfilm
 
-import android.util.Log
-import android.widget.Toast
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.annotation.NonNull
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -18,13 +19,25 @@ open class FCMService : FirebaseMessagingService() {
     override fun onMessageReceived(@NonNull remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
-        val title : String? = remoteMessage.data["title"]
-        val message : String? = remoteMessage.data["message"]
+        val title: String? = remoteMessage.data["title"]
+        val message: String? = remoteMessage.data["message"]
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val notificationManager : NotificationManager = getSystemService (Context.NOTIFICATION_SERVICE) as NotificationManager
+            val  notificationChannel = NotificationChannel(
+                "c207",
+                "c207",
+                NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationChannel.setDescription("c207");
+            notificationChannel.setShowBadge(true)
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
         val notificationBuilder = NotificationCompat.Builder(this, "c207")
             .setSmallIcon(R.drawable.logo).setContentTitle(title).setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-        NotificationManagerCompat.from(applicationContext).notify(1,notificationBuilder.build())
+        NotificationManagerCompat.from(applicationContext).notify(1, notificationBuilder.build())
     }
 }
