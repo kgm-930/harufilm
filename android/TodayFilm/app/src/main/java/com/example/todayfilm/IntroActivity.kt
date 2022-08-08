e1617d6da1b01015a9c66b7201f941fec96b45c3
kgm <72342542+kgm-930@users.noreply.github.com>
2022-08-08 13:35:04 +0900

    Update IntroActivity.kt

--- android/TodayFilm/app/src/main/java/com/example/todayfilm/IntroActivity.kt
+++ android/TodayFilm/app/src/main/java/com/example/todayfilm/IntroActivity.kt
@@ -3,9 +3,30 @@ package com.example.todayfilm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.example.todayfilm.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private var doubleBackToExit = false
+
+    override fun onBackPressed() {
+        if (doubleBackToExit) {
+            finishAffinity()
+        } else {
+            Toast.makeText(this, "종료하시려면 뒤로가기를 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
+            doubleBackToExit = true
+            runDelayed(1500L) {
+                doubleBackToExit = false
+            }
+        }
+    }
+
+    fun runDelayed(millis: Long, function: () -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed(function, millis)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
