bd5adcca985a2a6b7bc9e64c1c57c02232c00cd2
SSAFY <origene42@naver.com>
2022-08-10 13:25:30 +0900

    [248] fix

--- android/TodayFilm/app/src/main/java/com/example/todayfilm/ProfileFragment.kt
+++ android/TodayFilm/app/src/main/java/com/example/todayfilm/ProfileFragment.kt
@@ -9,8 +9,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import com.bumptech.glide.Glide
+import com.example.todayfilm.data.CompleteProfile
+import com.example.todayfilm.data.FindPwResponse
+import com.example.todayfilm.data.GetProfile
+import com.example.todayfilm.data.User
 import com.example.todayfilm.databinding.FragmentProfileBinding
+import com.example.todayfilm.retrofit.NetWorkClient
+import com.google.android.gms.common.internal.service.Common.API
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class ProfileFragment : Fragment(), View.OnClickListener {
@@ -20,6 +34,15 @@ class ProfileFragment : Fragment(), View.OnClickListener {
     var userdesc = ""
     var isMyProfile = true
     var isFollow = true
+
+
+
+
+
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -52,9 +75,15 @@ class ProfileFragment : Fragment(), View.OnClickListener {
     override fun onResume() {
         super.onResume()
         userid = MyPreference.read(requireContext(), "userid")
-
         username = MyPreference.read(requireContext(), "username")
         userdesc = MyPreference.read(requireContext(), "userdesc")
+
+
+
+
+
+
+
         if (username != ""){
             binding.profileUsername.setText(username)
         }
@@ -62,8 +91,81 @@ class ProfileFragment : Fragment(), View.OnClickListener {
             binding.profileDescription.setText(userdesc)
         }
 
-        val imgview = binding.profileImageFile
-        Glide.with(requireActivity()).load("http://i7c207.p.ssafy.io:8080/harufilm/profileimg/baseimg.png").into(imgview)
+
+
+
+
+
+
+
+
+
+
+        if (isMyProfile) {
+            val profile = GetProfile()
+            val pid = MyPreference.read(requireContext(), "userpid")
+            profile.userpid = pid.toString()
+
+
+            val call = NetWorkClient.GetNetwork.getprofile(profile)
+            call.enqueue(object : Callback<CompleteProfile>{
+
+                override fun onResponse(
+                    call: Call<CompleteProfile>,
+                    response: Response<CompleteProfile>
+                ) {
+                    Log.d("체크3",response.body()?.userimg.toString())
+                    val imgview = binding.profileImageFile
+                    Glide.with(requireActivity()).load("http://i7c207.p.ssafy.io:8080/harufilm/profileimg/"+response.body()?.userimg).into(imgview)
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+
+                override fun onFailure(call: Call<CompleteProfile>, t: Throwable) {
+
+
+
+                }
+
+
+
+
+
+            }
+            )
+
+
+
+
+
+        } else {
+            val imgview = binding.profileImageFile
+            Glide.with(requireActivity()).load("http://i7c207.p.ssafy.io:8080/harufilm/profileimg/baseimg.png").into(imgview)
+            // 본인 프로필이 아니라면 팔로우 중인지 확인
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
         setOnClickListener()
     }
@@ -109,5 +211,4 @@ class ProfileFragment : Fragment(), View.OnClickListener {
             }
         }
     }
-}
-
+////////////////////////////////////////////
