1fc14119b22141f418d1942f3da7571d3b909cb7
SSAFY <origene42@naver.com>
2022-08-17 18:26:47 +0900

    style 초안

--- android/TodayFilm/app/src/main/java/com/example/todayfilm/IntroActivity.kt
+++ android/TodayFilm/app/src/main/java/com/example/todayfilm/IntroActivity.kt
@@ -1,10 +1,13 @@
 package com.example.todayfilm
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.example.todayfilm.databinding.ActivityIntroBinding
 
@@ -32,6 +35,19 @@ class IntroActivity : AppCompatActivity() {
 
         val binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        @SuppressLint("ResourceType")
+        val animation_1 = AnimationUtils.loadAnimation(this,R.animator.alpha1)
+        @SuppressLint("ResourceType")
+        val animation_2 = AnimationUtils.loadAnimation(this,R.animator.alpha2)
+        @SuppressLint("ResourceType")
+        val animation_3 = AnimationUtils.loadAnimation(this,R.animator.alpha3)
+
+        binding.introOpen.startAnimation(animation_1)
+        binding.introTitle.startAnimation(animation_2)
+        binding.introBtn.startAnimation(animation_3)
+
+
 
         // settings에서 logout 버튼 눌러서 넘어온 경우, 내부 데이터 전부 삭제
         val logout = intent.getStringExtra("logout")
